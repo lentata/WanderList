3b8d45e284211845516c4214a651a74eb7f5b834
stephen-shelton <stephenshelton777@gmail.com>
2016-07-29 15:17:37 -0700

    [struct] set up structure to get hello world rendering to browser

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>TEST TEXT GOES HERE</div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
