188b09d20c1e5f87cf281b19b0895548be51fbf2
gabriellei814 <yihuilei814@gmail.com>
2016-07-30 09:15:36 -0700

    [config] add webpack hot reload

--- server.js
+++ server.js
@@ -3,6 +3,25 @@ var path = require('path');
 
 const app = express();
 
+(function() {
+  var webpack = require('webpack');
+  var webpackConfig = require(process.env.WEBPACK_CONFIG ? process.env.WEBPACK_CONFIG : './webpack.config');
+  var compiler = webpack(webpackConfig);
+
+  app.use(require("webpack-dev-middleware")(compiler, {
+    hot: true,
+    filename: 'bundle.js',
+    publicPath: '/assets',
+    stats: {
+      colors: true,
+    }
+  }));
+
+  app.use(require("webpack-hot-middleware")(compiler, {
+    log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
+  }));
+})();
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, function() {
