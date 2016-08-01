25c6423ba4ab35056c2f2df393c3d88685610f96
gabriellei814 <yihuilei814@gmail.com>
2016-08-01 16:00:38 -0700

    [config fix] fix bug using own server hot reload

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-var express = require('express');
 var path = require('path');
+var express = require('express');
+var webpack = require('webpack');
+var config = require('./webpack.config.dev');
 
 var app = express();
+var compiler = webpack(config);
 
-app.use(express.static(path.join(__dirname + '/public')));
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(3000, function() {
   console.log('server up at port 3000!!!! OMGEEEEEZ!!!');
---
+++ webpack.config.dev.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './client/index'
+  ],
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+    // js
+    {
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'client')
+    }
+    ]
+  }
+};
