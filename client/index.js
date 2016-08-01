f4ca109f87534d55c8d32a667d5f0fe991f55730
stephen-shelton <stephenshelton777@gmail.com>
2016-08-01 16:57:44 -0700

    [struct] created dummy data, loaded react-router-redux, modified combinedreducer, loaded redux dev tools

--- client/index.js
+++ client/index.js
@@ -2,15 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import Router from './routes';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
 import promise from 'redux-promise'
 
+import comments from './data/comments';
+import lists from './data/lists';
+
+
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
-const store = createStoreWithMiddleware(reducers);
+//initial dummy data for lists and comments
+const defaultState = {
+  lists,
+  // comments
+};
+
+//for redux dev tools
+const enhancers = compose(window.devToolsExtension ? window.devToolsExtension() : f => f);
+
+//tried to put defaultState (our data) and enhancers here
+const store = createStoreWithMiddleware(reducers, defaultState, enhancers);
+
+//browserHistory keeps track of app's pages you've been on
+const history = syncHistoryWithStore(browserHistory, store);
 
 if(module.hot) {
   module.hot.accept('./reducers/',() => {
@@ -19,16 +37,8 @@ if(module.hot) {
   });
 }
 
-//for redux dev tools
-const enhancers = compose(window.devToolsExtension ? window.devToolsExtension() : f => f);
-
+//Render our app!
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={ browserHistory} routes={routes} />
+    <Router history={history} routes={routes} />
   </Provider> , document.querySelector('.container'));
-
-
-//Hard Reload if statement
-// if (module.hot) {
-//   module.hot.accept();
-// }
