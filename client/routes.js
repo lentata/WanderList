0bccbd47129187528da29635e58f95561c7121ec
stephen-shelton <stephenshelton777@gmail.com>
2016-08-02 10:40:13 -0700

    [routes] revert routes back to old setup

--- client/routes.js
+++ client/routes.js
@@ -3,12 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import ListsIndex from './containers/lists';
 import ListDetail from './containers/list-detail';
+//need to import new list when that's created
 
 export default (
   <Route path='/' component={App}>
-    <IndexRoute component={ListsIndex}>
-      <Route path="/view/:listId" component={ ListDetail }></Route>
-    </IndexRoute>
+    <IndexRoute component={ ListsIndex } />
+    <Route path="/lists/:id" component={ ListDetail }></Route>
   </Route>
   );
 
