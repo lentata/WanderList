c1b2951181710e8a3fc621d6e3230abeda770af4
gabriellei814 <yihuilei814@gmail.com>
2016-08-02 09:54:50 -0700

    [server: feat] add dummy sqlite db user table, add server function handle login request

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,5 +1,7 @@
 var dummy = require('./dummy');
 var path = require('path');
+var Users = require('./collections/users');
+var User = require('./models/user');
 
 module.exports = function(app) {
   app.get('/', function(req, res) {
@@ -8,5 +10,20 @@ module.exports = function(app) {
 
   app.get('/api/posts', function(req, res) {
     res.send(dummy);
+  });
+
+  app.get('/api/auth', function(req, res) {
+    // console.log(req.query)
+    var info = req.query;
+    User.where({
+      username: info.username,
+      password: info.password
+    }).fetch().then(function(user) {
+      if(user) {
+        res.send(true);
+      } else {
+        res.send(false);
+      }
+    });
   });
 }
