65f8848a539d5fab1290c40bd7536d708b4a939e
George Mynatt <gmynatt.dali.cn@gmail.com>
2016-08-02 12:22:13 -0700

    login container

--- client/routes.js
+++ client/routes.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { render } from 'react-dom';
+
+
 import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import ListsIndex from './containers/lists';
 import ListDetail from './containers/list-detail';
 import Login from './containers/login';
+
 //need to import new list when that's created
 
 export default (
@@ -14,5 +18,6 @@ export default (
     <Route path='login' component={Login} />
   </Route>
 );
+
 
     // <Route path="lists/:id" component = {ShowList} />
