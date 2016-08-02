2673bbe2b36f376c97fefe59bf80e17d2286f50e
stephen-shelton <stephenshelton777@gmail.com>
2016-08-02 16:51:08 -0700

    [create list] fixed list creator to work with redux-form 4.1.3

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import { reducer as formReducer } from 'redux-form';
 import ListsReducer from './reducer_lists';
 import ActiveList from './reducer_active_list';
 
 const rootReducer = combineReducers({
   lists: ListsReducer,
   activeList: ActiveList,
+  form: formReducer,
   routing: routerReducer
 });
 
--- client/routes.js
+++ client/routes.js
@@ -5,24 +5,16 @@ import { render } from 'react-dom';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import ListsIndex from './containers/lists';
+import NewList from './containers/new_list';
 import ListDetail from './containers/list-detail';
 import Login from './containers/login';
 
-
-
-//need to import new list when that's created
-
 export default (
-  <Route path='/' component={App}>
+  <Route path='/' component={ App }>
     <IndexRoute component={ ListsIndex } />
+    <Route path="lists/new" component={ NewList } />
     <Route path="/lists/:id" component={ ListDetail }></Route>
-// import ListDetail from './containers/list-detail';
-    <Route path='login' component={Login} />
+    <Route path='login' component={ Login } />
   </Route>
 
 );
-
-
-
-
-    // <Route path="lists/:id" component = {ShowList} />
