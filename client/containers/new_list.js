02d3c4b16ac846d7af2cfd5c628fea1b2134d6ed
stephen-shelton <stephenshelton777@gmail.com>
2016-08-03 16:45:40 -0700

    [form] mostly fixed form, promise still not fully resolve but content is rendering to page

--- client/containers/new_list.js
+++ client/containers/new_list.js
@@ -4,21 +4,59 @@ import { createList } from '../actions/index';
 import { Link, browserHistory} from 'react-router';
 
 class ListsNew extends Component {
-  // static contextTypes = {
-  //   router: PropTypes.object
-  // }
 
   onSubmit(props) {
-    this.props.createList(props)
+    console.log('PROPS TEST', props);
+
+    var list = {
+      title: props.title,
+      author: 'somebody baaaad',
+      upvote: '0',
+      downvote: '0',
+      categories: [props.categories],
+      id: '6',
+      content: [
+        {headline: props.headline1,
+         img: props.image1,
+         desc: props.description1
+        }
+      ],
+      comments: [
+        {
+          user: '',
+          upvote: 0,
+          downvote: 0,
+          text: ''
+        }
+      ]
+    };
+
+    id++;
+
+    console.log('LIST TEST', list);
+
+    this.props.createList(list)
       .then(() => {
+        console.log('did this even run, bro?!');
         // this.context.router.push('/');
         //  this.props.router.push('/');
          browserHistory.push('/');
       })
   }
 
+  // ,headline2, image2, description2,
+  // headline3, image3, description3,
+  // headline4, image4, description4,
+  // headline5, image5, description5,
+  // headline6, image6, description6,
+  // headline7, image7, description7,
+  // headline8, image8, description8,
+  // headline9, image9, description9,
+  // headline10, image10, description10
+
   render() {
-    const { asyncValidating, fields: { title, categories, content }, handleSubmit, submitting } = this.props;
+    const { asyncValidating, fields: { title, categories,
+      headline1, image1, description1 }, handleSubmit, submitting } = this.props;
 
     return (
       <form onSubmit={ handleSubmit(this.onSubmit.bind(this))}>
@@ -43,11 +81,27 @@ class ListsNew extends Component {
           </div>
         </div>
 
-        <div className={`form-group ${content.touched && content.invalid ? 'has-error' : ''}`}>
-          <label className="control-label">Content*</label>
-          <textarea className="form-control" {...content} />
+        <div className={`form-group ${headline1.touched && headline1.invalid ? 'has-error' : ''}`}>
+          <label className="control-label">Headline 1*</label>
+          <input type="text" className="form-control" {...headline1} />
           <div className="help-block">
-            {content.touched ? content.error : ''}
+            {headline1.touched ? headline1.error : ''}
+          </div>
+        </div>
+
+        <div className={`form-group ${image1.touched && image1.invalid ? 'has-error' : ''}`}>
+          <label className="control-label">Image 1 URL*</label>
+          <input type="text" className="form-control" {...image1} />
+          <div className="help-block">
+            {image1.touched ? image1.error : ''}
+          </div>
+        </div>
+
+        <div className={`form-group ${description1.touched && description1.invalid ? 'has-error' : ''}`}>
+          <label className="control-label">Description 1*</label>
+          <textarea className="form-control" {...description1} />
+          <div className="help-block">
+            {description1.touched ? description1.error : ''}
           </div>
         </div>
 
@@ -68,188 +122,31 @@ function validate(values) {
   if (!values.categories || values.categories.trim() === '') {
     errors.categories = 'Enter categories';
   }
-  if (!values.content || values.content.trim() === '') {
-    errors.content = 'Enter some content';
+  if (!values.headline1 || values.headline1.trim() === '') {
+    errors.headline1 = 'Enter some content';
+  }
+  if (!values.image1 || values.image1.trim() === '') {
+    errors.image1 = 'Enter some content';
+  }
+  if (!values.description1 || values.description1.trim() === '') {
+    errors.description1 = 'Enter some content';
   }
 
   return errors;
 }
 
+// ,'headline2', 'image2', 'description2',
+// 'headline3', 'image3', 'description3',
+// 'headline4', 'image4', 'description4',
+// 'headline5', 'image5', 'description5',
+// 'headline6', 'image6', 'description6',
+// 'headline7', 'image7', 'description7',
+// 'headline8', 'image8', 'description8',
+// 'headline9', 'image9', 'description9',
+// 'headline10', 'image10', 'description10'
 
 export default reduxForm({
   form: 'ListsNewForm',
-  fields: ['title', 'categories', 'content'],
+  fields: ['title', 'categories', 'headline1', 'image1', 'description1'],
   validate
 }, null, { createList })(ListsNew);
-
-//WORKING VERSION FORM REDUX-FORM 4.2
-// import React, { Component, PropTypes } from 'react';
-// import { reduxForm } from 'redux-form';
-// import { createList } from '../actions/index';
-// import { Link, browserHistory} from 'react-router';
-//
-// class ListsNew extends Component {
-//   // static contextTypes = {
-//   //   router: PropTypes.object
-//   // }
-//
-//   onSubmit(props) {
-//     this.props.createList(props)
-//       .then(() => {
-//         // this.context.router.push('/');
-//         //  this.props.router.push('/');
-//          browserHistory.push('/');
-//       })
-//   }
-//
-//   render() {
-//     const { fields: { title, categories, content }, handleSubmit } = this.props;
-//
-//     return (
-//       <form onSubmit={ handleSubmit(this.onSubmit.bind(this))}>
-//         <h3>Create a New List</h3>
-//
-//         <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
-//           <label>Title</label>
-//           <input type="text" className="form-control" { ...title } />
-//           <div className="text-help">
-//             { title.touched ? title.error : '' }
-//           </div>
-//         </div>
-//
-//         <div className={ `form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}` }>
-//           <label>Categories</label>
-//           <input type="text" className="form-control" { ...categories } />
-//           <div className="text-help">
-//             { categories.touched ? categories.error : '' }
-//           </div>
-//         </div>
-//
-//         <div className={ `form-group ${content.touched && content.invalid ? 'has-danger' : ''}` }>
-//           <label>Content</label>
-//           <textarea className="form-control" { ...content } />
-//           <div className="text-help">
-//             { content.touched ? content.error : '' }
-//           </div>
-//         </div>
-//
-//         <button type="submit" className="btn btn-primary">
-//           Submit
-//         </button>
-//         <Link to="/" className="btn btn-danger">Cancel</Link>
-//       </form>
-//     );
-//   }
-// }
-//
-// function validate(values) {
-//   const errors = {};
-//
-//   if(!values.title) {
-//     errors.title = "Enter a title";
-//   }
-//   if(!values.categories) {
-//     errors.categories = "Enter categories";
-//   }
-//   if(!values.content) {
-//     errors.content = "Enter top list!";
-//   }
-//
-//   return errors;
-// }
-//
-//
-// export default reduxForm({
-//   form: 'ListsNewForm',
-//   fields: ['title', 'categories', 'content'],
-//   validate
-// }, null, { createList })(ListsNew);
-
-//ATTEMPT USING REDUX-FORM 6.00, WILL PROBABLY DELETE
-// import React, { Component, PropTypes } from 'react';
-// import { Field, reduxForm } from 'redux-form';
-// import { createList } from '../actions/index';
-// import { Link, browserHistory, withRouter } from 'react-router';
-//
-// class ListsNew extends Component {
-//   // static contextTypes = {
-//   //   router: PropTypes.object.isRequired
-//   // }
-//
-//   onSubmit(props) {
-//     this.props.createList(props)
-//       .then(() => {
-//         // this.context.router.push('/');
-//         // this.props.router.push('/');
-//         browserHistory.push('/');
-//       })
-//   }
-//
-//   render() {
-//
-//     const { handleSubmit, pristine, reset, submitting } = this.props;
-//     return (
-//       <form onSubmit={handleSubmit}>
-//         <div className="form-group">
-//           <label>Title</label>
-//           <Field name="title" component={title =>
-//             <div>
-//               <input type="text" className="form-control" {...title} placeholder="title of list here" />
-//               {title.touched && title.error && <span>{title.error}</span>}
-//             </div>
-//           }/>
-//         </div>
-//         <div className="form-group">
-//           <label>Categories</label>
-//           <Field name="categories" component={categories =>
-//             <div>
-//               <input type="text" className="form-control" {...categories} placeholder="categories of list here" />
-//               {categories.touched && categories.error && <span>{categories.error}</span>}
-//             </div>
-//           }/>
-//         </div>
-//         <div className="form-group">
-//           <label>Content</label>
-//           <Field name="content" component={content =>
-//             <div>
-//               <textarea className="form-control" {...content} placeholder="content of list here" />
-//               {content.touched && content.error && <span>{content.error}</span>}
-//             </div>
-//           }/>
-//         </div>
-//         <button type="submit" disabled={submitting}>Submit</button>
-//         <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
-//       </form>
-//
-//
-//
-//
-//     );
-//   }
-// }
-//
-// export default function validate(values) {
-//   const errors = {};
-//
-//   if(!values.title) {
-//     errors.title = "Enter a title";
-//   }
-//   if(!values.categories) {
-//     errors.categories = "Enter categories";
-//   }
-//   if(!values.content) {
-//     errors.content = "Enter top list!";
-//   }
-//   return errors;
-// }
-//
-// // module.exports = withRouter(ListsNew);
-//
-// export default reduxForm({
-//   form: 'ListsNewForm',
-//   validate
-// }, null, { createList })(ListsNew);
-//
-//
-//
-//
