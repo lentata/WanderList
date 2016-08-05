ab042ac02f74cee5fb5cc959a4f41a04a6863747
Lee Tsai <lee.x.tsai@gmail.com>
2016-08-05 10:41:42 -0700

    [Components/Reducers: Refactor] Code clean-up. Deleted obsolete component and reducers.

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,12 +2,10 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { reducer as formReducer } from 'redux-form';
 import ListsReducer from './reducer_lists';
-import ActiveList from './reducer_active_list';
 import AuthReducer from './reducer_auth';
 
 const rootReducer = combineReducers({
   lists: ListsReducer,
-  activeList: ActiveList,
   form: formReducer,
   routing: routerReducer,
   auth: AuthReducer
--- client/reducers/reducer_active_list.js
+++
@@ -1,8 +0,0 @@
-//MARKED FOR DELETION
-export default function(state = null, action) {
-  if(action.type === 'LIST_SELECTED') {
-    return action.payload;
-  } else {
-    return state;
-  }
-}
