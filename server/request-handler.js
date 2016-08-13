54198516007b105b2b6396f71db3199838409817
Lee Tsai <lee.x.tsai@gmail.com>
2016-08-12 20:01:24 -0700

    Update request-handler.js

--- server/request-handler.js
+++ server/request-handler.js
@@ -82,7 +82,7 @@ module.exports = function(app) {
 
     new List(posted).save(function(err){
       if (err) throw err;
-      res.status(200).json(posted);
+      res.status(201).json(posted);
     });
   });
 
