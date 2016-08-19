18f23780e597ab71a2378834856bc154300a024b
stephen-shelton <stephenshelton777@gmail.com>
2016-08-18 19:35:23 -0700

    [Component: Refactor] WIP Fixing search results page

--- client/components/searchPage.js
+++ client/components/searchPage.js
@@ -4,28 +4,27 @@ import { fetchedSearchLists, fetchedSearchCategories } from '../actions/index';
 import { bindActionCreators } from 'redux';
 import NavBar from './nav';
 import List from '../containers/lists';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 export class SearchPage extends Component {
   constructor(props) {
     super(props);
+      this.renderSearchedList = this.renderSearchedList.bind(this);
+      this.renderSearchedCat = this.renderSearchedCat.bind(this);
+  }
+
+  handleSelect(index, last) {
+    console.log('Selected tab: ' + index + ', Last tab: ' + last);
   }
 
   componentWillMount() {
-    this.props.fetchedSearchLists(this.props.params.searchedTerm);
-    this.props.fetchedSearchCategories(this.props.params.searchedTerm);
+    this.props.fetchedSearchLists(this.props.params.searchedTerm)
+    .then(()=>console.log("BLAHHHHH"));
+    this.props.fetchedSearchCategories(this.props.params.searchedTerm).then(()=>console.log("GLAHHHHHZ"));
   }
 
-  renderSearchedList(arr) {
-    var out = [];
-    for(var piece in arr) {
-      out.push(arr[piece]);
-    }
-    if(!out.length){
-      return "NOTHING FOUND";
-    }
-
-    console.log('PROPS BEFORE RENDER: ', this.props)
-    return out.map((list, i) => <List {...this.props}
+  renderSearchedList() {
+    return this.props.searchLists.map((list, i) => <List {...this.props}
       info={this.props.info}
       votes={list.upvote - list.downvote}
       upLists={this.props.upLists}
@@ -38,20 +37,35 @@ export class SearchPage extends Component {
   }
 
   render(){
+    console.log('CATS:', this.props.searchCats);
+    console.log('LISTS:', this.props.searchLists);
+
     const { list, info } = this.props;
-    return(
+    if (!this.props.searchCats || !this.props.searchLists) {
+      return <div></div>
+    }
+    return (
       <div>
         <NavBar />
-        <ul>
-          <h1>By Categories: {this.props.params.searchedTerm}</h1>
-          <div className="search_by_cat_container">
-            {this.renderSearchedList(this.props.searchCats)}
-          </div>
-          <h1>By Title: {this.props.params.searchedTerm}</h1>
-          <div className="search_by_title_container">
-            {this.renderSearchedList(this.props.searchLists)}
-          </div>
-        </ul>
+
+        <Tabs onSelect={this.handleSelect}>
+          <TabList>
+            <Tab>Lists by Categories</Tab>
+            <Tab>Lists by Headlines</Tab>
+          </TabList>
+          <TabPanel>
+            <h1>By Categories: {this.props.params.searchedTerm}</h1>
+            <div className="search_by_cat_container">
+              {this.renderSearchedCat(this.props.searchCats)}
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <h1>By Title: {this.props.params.searchedTerm}</h1>
+            <div className="search_by_title_container">
+              {this.renderSearchedList(this.props.searchLists)}
+            </div>
+          </TabPanel>
+        </Tabs>
       </div>
     );
   }
@@ -59,14 +73,14 @@ export class SearchPage extends Component {
 
 function mapStateToProps(state) {
   return {
+    searchLists: state.lists.searchLists,
+    searchCats: state.lists.searchCats,
     list: state.lists.all,
     info: state.lists.info,
     upLists: state.lists.upvotedLists,
     downLists: state.lists.downvotedLists,
     favoriteLists: state.lists.favoriteLists,
-    categoryLists: state.lists.categoryLists,
-    searchLists: state.lists.searchLists,
-    searchCats: state.lists.searchCats
+    categoryLists: state.lists.categoryLists
   };
 }
 
